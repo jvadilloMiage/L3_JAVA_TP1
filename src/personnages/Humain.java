1f70c93beb6993642554bd446fe3338f018674d0
coloc <coloc@Omen-Julian.private.univ-tlse3.fr>
2024-11-07 15:26:07 +0100

    v1 07/11/2024

---
+++ src/personnages/Humain.java
@@ -0,0 +1,46 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private int argent;
+	private String boisson;
+	
+	public Humain(String nom,int argent,String boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(this.nom+": "+texte);
+	}
+	
+	public void bonjour() {
+		parler("Bonjour ! Je m'appelle "+this.nom+" et j'aime boire du "+this.boisson);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de "+this.boisson+" ! GLOUPS !");
+	}
+	
+	public int getArgent() {
+		return this.argent;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public String boisson() {
+		return this.boisson;
+	}
+	
+	public void gagnerArgent(int n) {
+		this.argent += n;
+	}
+	
+	public void perdreArgent(int n) {
+		this.argent -= n;
+	}
+	
+}
